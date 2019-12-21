b89bb79d5ac69f1a51bf894fdc2f4aaa7ee062fe
Isac Sund <isac.sund@outlook.com>
2019-12-21 15:59:32 +0200

    Change post migration to have body as text

--- priv/repo/migrations/20191221104827_create_posts.exs
+++ priv/repo/migrations/20191221104827_create_posts.exs
@@ -4,7 +4,7 @@ defmodule Kamleague.Repo.Migrations.CreatePosts do
   def change do
     create table(:posts) do
       add :title, :string
-      add :body, :string
+      add :body, :text
       add :user_id, references(:users, on_delete: :nothing)
 
       timestamps()
