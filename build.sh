a332f26ad23a4f294c0e4edd72f3d8ca0c92a62d
Isac Sund <isac.sund@outlook.com>
2019-12-26 23:04:15 +0200

    Change env to prod when running phx.digest

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ MIX_ENV=prod mix compile
 # Compile assets
 npm install --prefix ./assets
 npm run deploy --prefix ./assets
-mix phx.digest
+MIX_ENV=prod mix phx.digest
 
 # Build the release and overwrite the existing release directory
 MIX_ENV=prod mix release --overwrite
